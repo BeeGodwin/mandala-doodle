305a0c4d531115cc83ee3e4bf07cebe54b48ec8c
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-11-28 20:02:41 +0000

    chucked it all and started over

--- branch.py
+++ branch.py
@@ -1,119 +1,43 @@
-import random
 import math
 import numpy
 
+class Point:
+    """base class so our initial branch can have a parent."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-class Branch:
 
-    def __init__(self, ori=[0, 0], vct=(0, -1), dist=1.0, n=6, p=0.5, dep=0):
+class Branch(Point):
 
-        max_depth = 3
-        self.ori = ori
-        self.vct = vct
-        self.dist = dist
-        self.n = n
-        self.dens = self.dist / self.n
-        self.p = p
-        self.dep = dep + 1
+    def __init__(self, parent, dep=1, angle=0, dst=100, maxdep=7):
 
-        self.angles = self.get_angles()
-        self.nodes = self.place_nodes()
+        self.dep = dep
 
-        self.branches = []
-        if self.dep < max_depth:
-            self.branches = [self.place_branches(node) for node in self.nodes]
-
-    def place_nodes(self):
-        """returns a list of (x, y) tuples of points along this branch. dens says
-        how many pixels apart nodes should be. prob is the likelihood of a given
-        node being added to the returned list."""
-        nodes = []
-        x_gap, y_gap = self.vct[0] * self.dens, self.vct[1] * self.dens
-        node = (self.ori[0] + x_gap, self.ori[1] + y_gap)
-        for i in range(int((self.dist // self.dens)) - 1):
-            if self.choose():
-                nodes.append(node)
-            node = (node[0] + x_gap, node[1] + y_gap)
-        return nodes
-
-    def get_angles(self):
-        """returns a list of tuples of (x, y) tuples, describing normalised
-        vectors of the angles that the branches off this branch will produce."""
-        my_angle = self.polar(self.vct)
-        pairs = (self.n - 1) // 2
-        angles = []
-        angle_inc = 360 / self.n
-        for i in range(pairs):
-            pair = [my_angle + angle_inc * (i + 1), my_angle - angle_inc * (i + 1)]
-            angles.append([self.cartesian(pair[0]), self.cartesian(pair[1])])
-        return angles
-
-    def choose(self):
-        """uses self.prob to choose true/false."""
-        # if self.dist < self.dens:
-        #     return False
-        if random.random() < self.p:
-            return True
+        if self.dep == 1:
+            self.x, self.y = parent.x, parent.y
         else:
-            return False
+            self.x, self.y = parent.end_x, parent.end_y
+            # also tell my parent I am its child
 
-    def place_branches(self, node):
-        """instantiates a pair(s) of new branches at the given node, and returns
-        a list of pairs of branches."""
-        if len(self.angles) == 1:
-            branches = self.branch_gen(node, self.angles[0])
-            return branches
-        else:
-            j = random.randrange(0, len(self.angles) - 1)
-            branches = self.branch_gen(node, self.angles[j])
-        return branches
+        self.angle = angle
+        self.dst = dst
 
-    def branch_gen(self, node, angles):
-        """parametrises & instantiates one pair of new branches."""
-        sd = random.randrange(random.getrandbits(32))
-        _len = self.dist - self.dens
-        _den = self.dens
-        if self.choose():
-            _len /= 2
-            _den /= 2
+        # self.end_x, self.end_y = # some function
 
-        random.seed(sd)
-        branch_a = Branch(ori=node, vct=angles[0], dist=_len, p=self.p, n=self.n, dep=self.dep)
-        # print('New branch at {} with vec {} and leng {}'.format(branch_a.ori, branch_a.vec, branch_a.leng))
-        random.seed(sd)
-        branch_b = Branch(ori=node, vct=angles[1], dist=_len, p=self.p, n=self.n, dep=self.dep)
-        # print('New branch at {} with vec {} and leng {}'.format(branch_b.ori, branch_b.vec, branch_b.leng))
-        return [branch_a, branch_b]
+        self.chn = []
 
-    @staticmethod
-    def polar(vct):
-        """given a normalised (x, y) vector, return its angle (with 0 being
-        considered vertical, on a scale 0-359.99.)"""
-        phi = math.degrees(numpy.arctan2(vct[0], -vct[1]))
-        if vct[0] < 0:
-            phi += 360
-        return round(phi, 3)
+        if self.dep < maxdep:
+            pass  # recurse
 
-    @staticmethod
-    def cartesian(phi):
-        """given an angle phi, return a normalised (x, y) vector as a tuple of
-        floats."""
-        x = round(math.sin(math.radians(phi)), 3)
-        y = -round(math.cos(math.radians(phi)), 3)
-        return x, y
+    def set_angle(self):
+        """gets my parent angle and my current relative angle to compute
+        my absolute angle."""
+        # if I'm depth 1, I have no parent and my relative angle is absolute
+        # otherwise compute current relative angle
+        # set my new end point
+        pass
 
-    def move(self, move_vct):
-        """move origin by move_vct, update node positions,
-        and pass to children."""
-        self.ori[0] += move_vct[0]
-        self.ori[1] += move_vct[1]
-        # do the same for all my node positions
-        # then pass the node positions downstream
+    def add_child(self, child_branch):
+        self.chn.append(child_branch)
 
-    def rotate(self, angle):
-        """rotate branch by angle. Recalculate node positions,
-        pass to children"""
-        my_angle = self.polar(self.vct)
-        my_angle += angle
-        self.vct = self.cartesian(my_angle)
-        # more loopage
--- play_mandala.py
+++ play_mandala.py
@@ -2,8 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 
-from branch import Branch
-
+from branch import Branch, Point
 
 def main():
     pygame.init()
@@ -11,17 +10,14 @@ def main():
     res = wid, hi = 640, 480
     d_surf = pygame.display.set_mode(res)
 
-    branch = Branch(ori=[wid/2, hi/2], dist=100, n=4, p=0.5)
+    point = Point(wid / 2, hi / 2)
+
+    branch = Branch(point)
 
     while True:
 
         d_surf.fill((0, 0, 0))
 
-        draw_branch(branch, d_surf, 7)
-
-        # branch.move([1, 0])
-
-        branch.rotate(1)
 
         for e in pygame.event.get():
             if e.type == QUIT:
@@ -29,15 +25,6 @@ def main():
                 sys.exit()
 
         pygame.display.update()
-
-
-def draw_branch(branch, d_surf, weight):
-    end_x = branch.ori[0] + (branch.vct[0] * branch.dist)
-    end_y = branch.ori[1] + (branch.vct[1] * branch.dist)
-    pygame.draw.line(d_surf, (255, 255, 255), branch.ori, (end_x, end_y), weight)
-    for pair in branch.branches:
-        for child_branch in pair:
-            draw_branch(child_branch, d_surf, weight - 2)
 
 
 if __name__ == '__main__':
