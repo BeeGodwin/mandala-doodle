342144360988b44f63a59beabd51f7704ff0658c
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-11-29 15:46:23 +0000

    gradualy working out how to propagate change thru the structure

--- branch.py
+++ branch.py
@@ -1,5 +1,5 @@
 import math
-import numpy
+
 
 class Point:
     """base class so our initial branch can have a parent."""
@@ -7,14 +7,19 @@ class Point:
         self.x = x
         self.y = y
 
+    # TODO- make a propagate method that does all the post-creation recursion.
+
 
 class Branch(Point):
 
-    def __init__(self, parent, dep=1, angle=0, dev=10, dst=100, max_dep=7):
+    def __init__(self, parent, dep=1, angle=0, dev=10, dst=100, max_dep=5):
+
+        self.parent = parent
 
         self.dep = dep
         self.max_dep = max_dep
         self.dev = dev
+        # self.pos = pos
 
         if self.dep == 1:
             self.x, self.y = parent.x, parent.y
@@ -30,30 +35,44 @@ class Branch(Point):
         self.chn = []
 
         if self.dep < max_dep:
+            ch_dst = self.dst - ((1 / self.max_dep) * self.dst)
             child_a = Branch(self,
                              dep=self.dep + 1,
-                             dst=int(self.dst / 2),
-                             angle=self.angle - dev,
+                             dst=int(ch_dst),  # changing these vals
+                             angle=self.angle - dev,  # in opposite direction?
                              max_dep=self.max_dep,
                              dev=self.dev)
             child_b = Branch(self,
                              dep=self.dep + 1,
-                             dst=int(self.dst / 2),
+                             dst=int(ch_dst),
                              angle=self.angle + dev,
                              max_dep=self.max_dep,
                              dev=self.dev)
             self.add_child(child_a)
             self.add_child(child_b)
 
-    def set_angle(self):
-        """gets my parent angle and my current relative angle to compute
-        my absolute angle."""
-        # if I'm depth 1, I have no parent and my relative angle is absolute
-        # otherwise compute current relative angle
-        # set my new end point
-        pass
+    def inc_angle(self, degrees, dep):
+        """Modifies self.angle by angle at depth dep."""
+        if dep == self.dep:
+            self.angle += degrees
+        else:
+            for ch in self.chn:
+                ch.inc_angle(degrees, dep)
+
+    def inc_dev(self, degrees, dep):
+        """Modifies self.dev by degrees at depth dep."""
+        if dep == self.dep:
+            self.dev += degrees
+        else:
+            for ch in self.chn:
+                ch.inc_dev(degrees, dep)
+
+    def set_position(self, x, y):
+        """Changes self.x and self.y to new values."""
+        self.x, self.y = x, y
 
     def add_child(self, child_branch):
+        """Adds a child branch."""
         self.chn.append(child_branch)
 
     @staticmethod
@@ -71,5 +90,22 @@ class Branch(Point):
         end_x = vct_x * self.dst + self.x
         end_y = vct_y * self.dst + self.y
         return end_x, end_y
+
+    def propagate(self):
+        """Re-evaluate my own state, and recursively tell child branches
+        to update theirs."""
+        self.x, self.y = self.get_parent_origin()
+        # self.angle = angle
+        # self.dev = dev
+        self.end_x, self.end_y = self.find_endpoint()
+        if len(self.chn) > 0:
+            self.chn[0].propagate()
+            self.chn[1].propagate()
+
+    def get_parent_origin(self):
+        """get my x and y from parent branch or point."""
+        if self.dep == 1:
+            return self.parent.x, self.parent.y
+        return self.parent.end_x, self.parent.end_y
 
 
--- play_mandala.py
+++ play_mandala.py
@@ -4,21 +4,30 @@ from pygame.locals import *
 
 from branch import Branch, Point
 
+
 def main():
     pygame.init()
 
-    res = wid, hi = 640, 480
+    res = wid, hi = 1024, 768
     d_surf = pygame.display.set_mode(res)
 
-    point = Point(wid / 2, hi)
-
-    branch = Branch(point, dev=30, dst=300)
+    point = Point(wid / 2, hi / 2)
+    n = 9
+    branch_list = []
+    for i in range(n):
+        branch = Branch(point, angle=360 / n * i, dev=27, dst=100, max_dep=5)
+        branch_list.append(branch)
 
     while True:
 
         d_surf.fill((0, 0, 0))
-
-        draw_branch(d_surf, branch)
+        for branch in branch_list:
+            draw_branch(d_surf, branch)
+            # branch.inc_angle(0.3, 3)
+            # branch.inc_angle(-0.1, 1)
+            # branch.inc_angle(0.05, 4)
+            branch.inc_dev(1, 2)
+            branch.propagate()
 
         for e in pygame.event.get():
             if e.type == QUIT:
