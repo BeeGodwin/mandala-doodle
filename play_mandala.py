e653d978a8a0479dd6d321d0cb2a1671a71ff872
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-11-30 22:58:31 +0000

    more drawing functions in play_mandala

--- play_mandala.py
+++ play_mandala.py
@@ -12,10 +12,11 @@ def main():
     d_surf = pygame.display.set_mode(res)
 
     point = Point(wid / 2, hi / 2)
-    n = 5
+    n = 7
+    max_dep = 5
     branch_list = []
     for i in range(n):
-        branch = Branch(point, angle=360 / n * i, dev=90 / n, dst=100, max_dep=6)
+        branch = Branch(point, angle=360 / n * i, dev=90 / n, dst=150, max_dep=max_dep)
         branch_list.append(branch)
 
     while True:
@@ -29,7 +30,8 @@ def main():
             branch.inc_tree_dev(0.04)  # then angle ops, tree-wide first
             branch.inc_dev(-0.4, 2)  # then dev ops, tree-wide first.
             branch.propagate()  # then propagate.
-            draw_branch(d_surf, branch)
+            # draw_branch(d_surf, branch)
+            draw_circles(d_surf, branch, max_dep)
 
         for e in pygame.event.get():
             if e.type == QUIT or e.type == KEYDOWN and e.key == 27:
@@ -46,9 +48,22 @@ def draw_branch(d_surf, branch):
                        (branch.x, branch.y),
                        (branch.end_x, branch.end_y),
                        1)
-    if len(branch.chn) > 0:
-        for ch in branch.chn:
-            draw_branch(d_surf, ch)
+    for ch in branch.chn:
+        draw_branch(d_surf, ch)
+
+
+def draw_circles(d_surf, branch, size):
+    """draws circles around points recursively"""
+    step_size = 255 / size
+    for i in range(size):
+        shade = i * step_size
+        pygame.draw.circle(d_surf,
+                           (shade, shade, shade),
+                           (int(branch.end_x), int(branch.end_y)),
+                           (size - i) * 3,
+                           1)
+    for ch in branch.chn:
+        draw_circles(d_surf, ch, size - 1)
 
 
 if __name__ == '__main__':
